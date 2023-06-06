8d30ddf640aa376f4dcb1c4a2054a173f5fc7581
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2023-06-06 15:24:09 +0200

    initial model

--- src/models/project1/networks.py
+++ src/models/project1/networks.py
@@ -19,5 +19,36 @@ def get_network(network_name: str):
             ('relu4', nn.ReLU()),
             ('fc2', nn.Linear(256, 2))
         ]))
+
+    elif network_name == 'initial':
+        return nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)),
+            ('relu1', nn.ReLU()),
+            ('pool1', nn.MaxPool2d(kernel_size=2, stride=2)),
+            ('dropout1', nn.Dropout(p=0.2)),
+            
+            ('conv2', nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)),
+            ('relu2', nn.ReLU()),
+            ('pool2', nn.MaxPool2d(kernel_size=2, stride=2)),
+            ('dropout2', nn.Dropout(p=0.2)),
+            
+            ('conv3', nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)),
+            ('relu3', nn.ReLU()),
+            ('pool3', nn.MaxPool2d(kernel_size=2, stride=2)),
+            ('dropout3', nn.Dropout(p=0.2)),
+            
+            ('conv4', nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)),
+            ('relu4', nn.ReLU()),
+            ('pool4', nn.MaxPool2d(kernel_size=2, stride=2)),
+            ('dropout4', nn.Dropout(p=0.2)),
+            
+            ('flatten', nn.Flatten()),
+            ('fc1', nn.Linear(128 * 14 * 14, 256)),
+            ('relu4', nn.ReLU()),
+            ('dropout5', nn.Dropout(p=0.2)),
+            ('fc2', nn.Linear(256, 2))
+        ]))
+	
+
     else:
         raise NotImplementedError("Not defined yet...")
