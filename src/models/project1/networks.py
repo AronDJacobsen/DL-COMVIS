dbaae585cb4a46a6f5dc3ba77ad74372717b6334
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2023-06-07 15:05:39 +0200

    norm small fix

--- src/models/project1/networks.py
+++ src/models/project1/networks.py
@@ -14,6 +14,7 @@ def get_network(network_name: str, args):
             return (f'in{layer_num}', nn.InstanceNorm2d(num_features)),
         elif norm_type == 'none':
             pass
+            return (f'none{layer_num}', nn.Identity())
         else:
             raise NotImplemented('Normalization-technique not yet implemented')
 
