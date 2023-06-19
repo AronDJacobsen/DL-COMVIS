0216d7151697bacb38e948d7fb12b21b65878f55
philliphoejbjerg <philliphoejbjerg@gmail.com>
2023-06-19 13:53:02 +0200

    updated utils and predict step

--- src/models/project4/models.py
+++ src/models/project4/models.py
@@ -9,9 +9,7 @@ import timm
 from torchmetrics.classification import Accuracy
 from torchvision.ops import box_iou
 
-
-from src.utils import accuracy, IoU
-
+from src.utils import accuracy, IoU, plot_SS
 
 def get_model(model_name, args, loss_fun, optimizer, out=False, num_classes=2, region_size=(512,512)):
     if model_name == 'testnet':
@@ -174,45 +172,14 @@ class BaseModel(pl.LightningModule):
         self.log('mAP/val', mAP, batch_size=len(batch), prog_bar=True, logger=True)
 
     def predict_step(self, batch, batch_idx):
-        x, y = batch
-        # predicting
-        y_hat = self.forward(x)
-        
-        if batch_idx == 0:
-        
-            batch_size = len(x)
 
-            for k in range(batch_size):
-                plt.subplot(3, batch_size, k+1)
-                plt.imshow(np.rollaxis(x[k].detach().cpu().numpy(), 0, 3), cmap='gray')
-                plt.title('Real')
-                plt.axis('off')
+            # for each image
+            for i, (img, cat_id, bboxes_data, pred_bboxes_data) in enumerate(batch):
+                # for each bounding box
+                (bboxes, regions)           = bboxes_data
+                (pred_bboxes, pred_regions) = pred_bboxes_data
 
-                plt.subplot(3, batch_size, k+1+batch_size)
-                plt.imshow(y_hat[k, 0].detach().cpu().numpy(), cmap='gray')
-                plt.title('Output')
-                plt.axis('off')
-				
-                plt.subplot(3, batch_size, k+1+2*batch_size)                
-                plt.imshow(y[k, 0].detach().cpu().numpy().transpose(1,2,0), cmap='gray')
-                plt.title('Label')
-                plt.axis('off')
-
-            plt.savefig(f"{self.args.log_path}/{self.args.experiment_name}/{self.args.model_name}/prediction.png")
-
-        
-        if self.out:
-            for k in range(len(x)):
-                fig = plt.figure()
-                plt.imshow(y_hat[k, 0].detach().cpu().numpy(), cmap='gray')
-                plt.savefig(f"{self.args.log_path}/{self.args.experiment_name}/{self.args.model_name}/prediction_mask{k+self.offset}.png")
-                plt.close(fig)
-
-                fig = plt.figure()
-                plt.imshow(np.rollaxis(x[k].detach().cpu().numpy(), 0, 3), cmap='gray')
-                plt.savefig(f"{self.args.log_path}/{self.args.experiment_name}/{self.args.model_name}/prediction_img{k+self.offset}.png", bbox_inches='tight')
-                plt.close(fig)
-            self.offset += len(x)
+                plot_SS(img, pred_bboxes, i, batch_idx)
 
 
 class TestNet(BaseModel):
@@ -245,31 +212,11 @@ class EfficientNet(BaseModel):
     def __init__(self, args, loss_fun, optimizer, out, num_classes, region_size):
         super().__init__(args, loss_fun, optimizer, out, num_classes)
 
-        self.args = args
-        self.lr = args.lr
-        
         # Load model
         self.network = timm.create_model(args.model_name, pretrained=True, num_classes=self.num_classes)
         # num_classes for 28 categories + 1 background
         if args.percentage_to_freeze != -1.0:
             self.freeze_parameters(args.percentage_to_freeze)
-
-        # Define metrics and loss criterion
-        self.loss_fun = loss_fun
-        self.accuracy = Accuracy(task="multiclass", num_classes=self.num_classes).to(self.device)
-
-        # Set up logging option
-        self.model_checkpoint = ModelCheckpoint(
-            monitor = "val_loss",
-            verbose = args.verbose,
-            filename = "{epoch}_{val_loss:.4f}",
-        )
-        # Setup initial learning rate finder
-        self.lr_finder = LearningRateFinder(
-            min_lr=args.min_lr,
-            max_lr=args.max_lr,
-            num_training_steps=args.initial_lr_steps,
-        )
 
     def freeze_parameters(self, percentage_to_freeze):
         # Freeze weights
@@ -299,7 +246,7 @@ class EfficientNet(BaseModel):
     def forward(self, x):
         return self.network(x)
 
-        
+    """
     def training_step(self, batch, batch_idx):
         # Extract and process input
         x, y = batch
@@ -332,3 +279,4 @@ class EfficientNet(BaseModel):
         #the average across the epoch - [default:True]
         self.log("val_acc", acc, prog_bar=True, logger=True),
         self.log("val_loss", loss, prog_bar=True, logger=True)
+    """
