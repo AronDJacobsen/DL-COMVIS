5b98a82a941e5633c261c0753cd16d9d012404a3
albertkjoller <albert_k_jacobsen@hotmail.com>
2023-06-20 15:40:28 +0200

    results notebook - plots running

--- project4.sh
+++ project4.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#BSUB -J albertkjoller_efficientnet_v7
-#BSUB -o /work3/s194253/02514/project4_results/sh_logs/albertkjoller_efficientnet_v7.out
-#BSUB -e /work3/s194253/02514/project4_results/sh_errors/albertkjoller_efficientnet_v7.err
+#BSUB -J albertkjoller_efficientnet_crossentropy_resnet_v6
+#BSUB -o /work3/s194253/02514/project4_results/sh_logs/albertkjoller_efficientnet_crossentropy_resnet_v6.out
+#BSUB -e /work3/s194253/02514/project4_results/sh_errors/albertkjoller_efficientnet_crossentropy_resnet_v6.err
 #BSUB -n 6
-#BSUB -q gpuv100
+#BSUB -q gpua100
 #BSUB -gpu 'num=1:mode=exclusive_process'
 #BSUB -W 24:00
-#BSUB -R 'rusage[mem=30GB]'
+#BSUB -R 'rusage[mem=60GB]'
 #BSUB -R 'span[hosts=1]'
 #BSUB -B
 #BSUB -N
@@ -16,13 +16,13 @@ module load cuda/11.8
 source /work3/s194253/02514/DL-COMVIS/venv/bin/activate
 
 CUDA_VISIBLE_DEVICES=0 python src/models/project4/train_model.py \
-    --model_name efficientnet_b4 \
-    --experiment_name albertkjoller_efficientnet \
+    --model_name resnet18 \
+    --experiment_name albertkjoller_efficientnet_crossentropy_resnet \
     --data_path '/work3/s194253/02514/project4_results/data_wastedetection' \
-    --epochs 200 \
-    --batch_size 1 \
+    --epochs 20 \
+    --batch_size 32 \
     --use_super_categories True \
     --seed 0 \
-    --lr 5e-03 \
+    --lr 1e-06 \
 
 
